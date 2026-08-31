fe40f3224a86e725c04c74d1e0f9cb226eb011c4
brxndx64 <matheus.cardozzo02@gmail.com>
2026-08-31 11:33:17 -0300

    primeiro commit

---
+++ ola.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+
+	fmt.Println("agora voce tem ", math.Sqrt(7), "problemas")
+
+	fmt.Println("O numero é", rand.Intn(4))
+
+
+
+}
