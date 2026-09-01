e47cd5eba6f73bb57155f4311213105df35f4a0e
brxndx64 <matheus.cardozzo02@gmail.com>
2026-09-01 15:08:05 -0300

    tirei o \bar e coloquei nome na funcao de escrita e requisicao

--- projeto - 01/http-requests.go
+++ projeto - 01/http-requests.go
@@ -2,28 +2,25 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
+func WandR(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+
+		http.Error(w, "metodo nao permitido", http.StatusMethodNotAllowed)
+
+		return
+
+	}
+	fmt.Fprintln(w, "ok")
+}
+
 func main() {
 
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Ola, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-
-		if r.Method != http.MethodGet {
-
-			http.Error(w, "metodo nao permitido", http.StatusMethodNotAllowed)
-
-			return
-
-		}
-		fmt.Fprintln(w, "ok")
-	})
+	http.HandleFunc("/health", WandR)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
