fc9dd2da8de5bd30e1a541ffa3c263338e1980a6
brxndx64 <matheus.cardozzo02@gmail.com>
2026-09-03 20:48:59 -0300

    validacao das structs do json

--- projeto - 01/http-requests.go
+++ projeto - 01/http-requests.go
@@ -5,14 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Evento struct {
-	
-	ID          string `json:"id"`
-	Type        string `json:"type"`
-	Channel     string `json:"channel"`
-	Destination string `json:"destination"`
+	ID          string          `json:"id"`
+	Type        string          `json:"type"`
+	Channel     string          `json:"channel"`
+	Destination string          `json:"destination"`
 	Payload     json.RawMessage `json:"payload"`
 }
 
@@ -42,8 +42,34 @@ func notificationsHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewDecoder(r.Body).Decode(&evento); err != nil {
 
-		http.Error(w, "formato inválido", http.StatusBadRequest)
+		http.Error(w, "formato invalido", http.StatusBadRequest)
 		return
+	}
+
+	if strings.TrimSpace(evento.ID) == "" {
+		http.Error(w, "falta ID ", http.StatusBadRequest)
+		return
+	}
+
+	if strings.TrimSpace(evento.Type) == "" {
+		http.Error(w, "falta aprovacao ", http.StatusBadRequest)
+		return
+	}
+
+	if strings.TrimSpace(evento.Channel) == "" {
+		http.Error(w, "webhook nao concluido ", http.StatusBadRequest)
+		return
+	}
+
+	if strings.TrimSpace(evento.Destination) == "" {
+		http.Error(w, "destino nao encontrado ", http.StatusBadRequest)
+		return
+	}
+
+	if len(evento.Payload) == 0 {
+		http.Error(w, "falha no pagamento ", http.StatusBadRequest)
+		return
+
 	}
 
 	log.Printf("%+v", evento)
